223bb3e2e30ae72f0f30d2b0ba413bd2adf6354d
spencer-scw <spencer.scw@gmail.com>
2024-02-27 06:37:25 -0700

    tests are done, phase 3 completed

--- server/src/test/java/serviceTests/GameServiceTests.java
+++ server/src/test/java/serviceTests/GameServiceTests.java
@@ -1,17 +1,75 @@
 package serviceTests;
 
+import chess.ChessGame;
+import dataAccess.*;
+import model.JoinData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import service.GameService;
+import service.UserService;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameServiceTests {
-    @Test
-    void listGames() {
+    AuthDAO authDAO;
+    GameDAO gameDAO;
+    UserDAO userDAO;
+
+    GameService gameService;
+    UserService userService;
+
+    UserData greg;
+
+    @BeforeEach
+    void setup() {
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+        userDAO = new MemoryUserDAO();
+        gameService = new GameService(authDAO, gameDAO);
+        userService = new UserService(authDAO, userDAO);
+        greg = new UserData("Greg", "asphodel", "greg@aloe.com");
     }
 
     @Test
-    void createGame() {
+    void listGamesPositive() {
+        var authToken = userService.register(greg).authToken();
+        assertNotNull(gameService.listGames(authToken));
     }
 
     @Test
-    void joinGame() {
+    void listGamesNegative() {
+        assertNull(gameService.listGames("FakeAuth"));
+    }
+
+    @Test
+    void createGamePositive() {
+        var authToken = userService.register(greg).authToken();
+        assertNotNull(gameService.createGame(authToken, "Greg's game"));
+    }
+
+    @Test
+    void createGameNegative() {
+        assertNull(gameService.createGame("Fake Token", "Greg's game"));
+    }
+
+    @Test
+    void joinGamePositive() throws Exception {
+        var authToken = userService.register(greg).authToken();
+        var game = gameService.createGame(authToken, "Greg's game");
+
+        gameService.joinGame(authToken, new JoinData(ChessGame.TeamColor.WHITE, game.gameID()));
+    }
+
+    @Test
+    void joinGameNegative() {
+        var authToken = userService.register(greg).authToken();
+
+        assertThrows(
+                Exception.class,
+                () -> {
+                    gameService.joinGame(authToken, new JoinData(ChessGame.TeamColor.WHITE, 1234));
+                }
+        );
     }
 }
--- server/src/test/java/serviceTests/UserServiceTests.java
+++ server/src/test/java/serviceTests/UserServiceTests.java
@@ -1,17 +1,63 @@
 package serviceTests;
 
+import dataAccess.*;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import service.UserService;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTests {
-    @Test
-    void register() {
+    AuthDAO authDAO;
+    UserDAO userDAO;
+    UserService service;
+    @BeforeEach
+    void setup() {
+        authDAO = new MemoryAuthDAO();
+        userDAO = new MemoryUserDAO();
+        service = new UserService(authDAO, userDAO);
     }
 
     @Test
-    void login() {
+    void registerPositive() {
+        var greg = new UserData("Greg", "asphodel", "greg@aloe.com");
+        assertNotNull(service.register(greg));
     }
 
     @Test
-    void logout() {
+    void registerNegative() {
+        var greg = new UserData("Greg", "asphodel", "greg@aloe.com");
+        service.register(greg);
+        assertNull(service.register(greg));
+    }
+
+    @Test
+    void loginPositive() {
+        var greg = new UserData("Greg", "asphodel", "greg@aloe.com");
+        service.register(greg);
+        assertNotNull(service.login(greg));
+    }
+
+    @Test
+    void loginNegative() {
+        var greg = new UserData("Greg", "asphodel", "greg@aloe.com");
+        assertNull(service.login(greg));
+    }
+
+    @Test
+    void logoutPositive() {
+        var greg = new UserData("Greg", "asphodel", "greg@aloe.com");
+        var auth = service.register(greg);
+        assertTrue(service.logout(auth.authToken()));
+    }
+
+    @Test
+    void logoutNegative() {
+        var greg = new UserData("Greg", "asphodel", "greg@aloe.com");
+        var authToken = "fakeAuthToken";
+        assertFalse(service.logout(authToken));
     }
 }
