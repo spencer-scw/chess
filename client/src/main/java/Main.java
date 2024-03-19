b3e07a0b4bce55f514df50fbffa2541318a1d51e
spencer-scw <spencer.scw@gmail.com>
2024-03-18 21:19:44 -0600

    login working inasmuch as it can without being able to register

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,28 @@
 import chess.*;
+import ui.ChessClient;
+
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        var client = new ChessClient("localhost:8080");
+
+        boolean openSession = true;
+
+        while(openSession) {
+            System.out.print("> ");
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+
+            if (Objects.equals(line, "quit")) {
+                openSession = false;
+            }
+            System.out.println(client.eval(line));
+        }
+
     }
 }
--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,13 +1,24 @@
 package ui;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
 import java.util.Arrays;
+import java.util.Map;
 
 public class ChessClient {
     private final String serverURL;
     private State clientState;
 
     public ChessClient(String serverURL) {
-        this.serverURL = serverURL;
+        this.serverURL = String.format("http://%s", serverURL);
+
         clientState = State.SIGNEDOUT;
     }
 
@@ -17,7 +28,7 @@ public class ChessClient {
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
-                case "login" -> signIn(params);
+                case "login" -> logIn(params);
                 case "register" -> register(params);
 
                 case "logout" -> logOut(params);
@@ -61,25 +72,58 @@ public class ChessClient {
     }
 
     private String observeGame(String[] params) {
+        return "";
     }
 
     private String joinGame(String[] params) {
+        return "";
     }
 
     private String listGames() {
+        return "";
     }
 
     private String createGame(String[] params) {
+        return "";
     }
 
     private String logOut(String[] params) {
+        return "";
     }
 
     private String register(String[] params) {
         return null;
     }
 
-    private String signIn(String[] params) {
-        return null;
+    private String logIn(String[] params) {
+        HttpURLConnection http;
+        try {
+            URI uri = new URI(String.format("%s/session", serverURL));
+            http = (HttpURLConnection) uri.toURL().openConnection();
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+            http.addRequestProperty("Content-Type", "application/json");
+            var body = Map.of("username", params[0], "password", params[1]);
+            try (var outputStream = http.getOutputStream()) {
+                var jsonBody = new Gson().toJson(body);
+                outputStream.write(jsonBody.getBytes());
+            }
+
+            http.connect();
+
+            try (InputStream responseBody = http.getInputStream()) {
+                InputStreamReader inputStreamReader = new InputStreamReader(responseBody);
+                var output = new Gson().fromJson(inputStreamReader, Map.class);
+                return output.toString();
+            }
+
+        } catch (Exception e) {
+            if (e.getClass() != IOException.class) {
+                return "bad URL";
+            } else {
+                return "Incorrect username or password. Please try again.";
+            }
+        }
+
     }
 }
